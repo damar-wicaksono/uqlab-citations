9429476c8e16adc1a86a33d86611ca20706ca3e3
Damar Wicaksono <damar.wicaksono@gmail.com>
2019-10-15 14:30:36 +0200

    Remove unnecessary import statement

--- uq_citations/parse_bibtex.py
+++ uq_citations/parse_bibtex.py
@@ -2,7 +2,6 @@
 """Module with functions to parse BibTeX file of UQLab citations
 """
 import bibtexparser
-from bibtexparser.bibdatabase import BibDatabase
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
 
@@ -15,7 +14,8 @@ def parse_bibtex(bibtex_file):
     bib_parser.customization = convert_to_unicode
     bib_db = bibtexparser.load(bibtex_file, parser=bib_parser)
     
-    # Sort the list in reverse chronological order by year
+    # Sort the entries in reverse chronological order by year
     bib_db.entries.sort(key=lambda x: x['year'], reverse=True)
 
+    # Return the list of entries
     return bib_db.entries
