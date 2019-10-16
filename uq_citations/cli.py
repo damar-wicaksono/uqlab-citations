543a68748af52b4f5b0170d61fb2b671ee7d92b3
Damar Wicaksono <damar.wicaksono@gmail.com>
2019-10-16 13:43:10 +0200

    Revise the CLI with new options

--- uq_citations/cli.py
+++ uq_citations/cli.py
@@ -1,21 +1,23 @@
 import click
 
-from . import parse_bibtex
-from . import parse_biblist
+from .parse_bibtex import parse_bibtex
+from .parse_biblist import parse_biblist
+from .write_html import write_html
+from .constants import SUBJECTS_DICT
 
 @click.command()
 @click.argument("bibtex", type=click.File("rt"))
-@click.option("--html_head", help = "HTML head template.")
-@click.option("--html_list", help = "HTML list template.")
+@click.option("--html_tpl", default="./html_tpl.html", help = "HTML template.")
+@click.option("--html_list", default="./bib_list.html", help = "HTML list template.")
 @click.option("--output", default="./output.html", help="HTML output filename.")
-def bibtex2html(bibtex, html_head, html_list, output):
-    """Convert BibTeX file of UQLab citations to a HTML file"""
-    bib_db_entries = parse_bibtex.parse_bibtex(bibtex)
+def bibtex2html(bibtex, html_tpl, html_list, output):
+    """Convert BibTeX file of UQLab citations to a HTML file."""
+    bib_db_entries = parse_bibtex(bibtex)
     
-    biblist_entries = parse_biblist.parse_biblist(bib_db_entries)
+    biblist_entries = parse_biblist(bib_db_entries)
     
-    for entry in biblist_entries:
-        print(entry["subject"])
+    write_html(biblist_entries, SUBJECTS_DICT, html_tpl, html_list, output)
+    
 
 if __name__ == "__main__":
     bibtex2html()
