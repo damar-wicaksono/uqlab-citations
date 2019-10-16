d2be90314321809a0e6f82516acd217529bed4cc
Damar Wicaksono <damar.wicaksono@gmail.com>
2019-10-16 13:53:57 +0200

    Show the default values for each option

--- uq_citations/cli.py
+++ uq_citations/cli.py
@@ -7,17 +7,29 @@ from .constants import SUBJECTS_DICT
 
 @click.command()
 @click.argument("bibtex", type=click.File("rt"))
-@click.option("--html_tpl", default="./html_tpl.html", help = "HTML template.")
-@click.option("--html_list", default="./bib_list.html", help = "HTML list template.")
-@click.option("--output", default="./output.html", help="HTML output filename.")
+@click.option(
+    "--html_tpl",
+    default="./html_tpl.html",
+    help = "HTML template.",
+    show_default=True)
+@click.option(
+    "--html_list",
+    default="./bib_list.html",
+    help = "HTML list template.",
+    show_default=True)
+@click.option(
+    "--output",
+    default="./output.html",
+    help="HTML output filename.",
+    show_default=True)
 def bibtex2html(bibtex, html_tpl, html_list, output):
     """Convert BibTeX file of UQLab citations to a HTML file."""
+    
+    # Parse the BibTeX file and return a list
     bib_db_entries = parse_bibtex(bibtex)
     
+    # Process the list of citations
     biblist_entries = parse_biblist(bib_db_entries)
     
+    # Write the list of citations into a file
     write_html(biblist_entries, SUBJECTS_DICT, html_tpl, html_list, output)
-    
-
-if __name__ == "__main__":
-    bibtex2html()
