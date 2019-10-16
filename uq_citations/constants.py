8418f6837125aae78f9521453298ecac197e11e2
Damar Wicaksono <damar.wicaksono@gmail.com>
2019-10-16 13:43:53 +0200

    Add module with constant values (subjects)

---
+++ uq_citations/constants.py
@@ -0,0 +1,26 @@
+SUBJECTS_DICT = [
+    {"name": "life sciences",
+     "members": ["biology",
+                 "biomedical science"],
+     "splitgroup": False
+     },
+    {"name": "computational science and engineering",
+     "splitgroup": False
+     },
+    {"name": "engineering applications",
+     "members": ["chemical engineering",
+                 "civil engineering",
+                 "electrical engineering",
+                 "energy engineering",
+                 "mechanical engineering",
+                 "monitoring and remote sensing",
+                 "nuclear engineering",
+                 "ocean engineering",
+                 ],
+     "splitgroup": True
+     },
+    {"name": "geosciences and environmental engineering",
+     "members": ["geoscience", "geomechanics", "environmental engineering"],
+     "splitgroup": False
+    }
+    ]
