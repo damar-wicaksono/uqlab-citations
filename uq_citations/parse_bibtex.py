5117d11c804d6a23d39f98f98bbfbfc57fc6aa18
Damar Wicaksono <damar.wicaksono@outlook.com>
2019-10-17 10:05:44 +0200

    Update module to conform with PEP8

--- uq_citations/parse_bibtex.py
+++ uq_citations/parse_bibtex.py
@@ -9,11 +9,12 @@ __author__ = "Damar Wicaksono"
 
 
 def parse_bibtex(bibtex_file):
-    """Parse BibTeX file of citations and return a list"""
+    """Parse BibTeX file of citations and return a list."""
+
     bib_parser = BibTexParser()
     bib_parser.customization = convert_to_unicode
     bib_db = bibtexparser.load(bibtex_file, parser=bib_parser)
-    
+
     # Sort the entries in reverse chronological order by year
     bib_db.entries.sort(key=lambda x: x['year'], reverse=True)
 
